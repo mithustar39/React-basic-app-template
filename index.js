48fcc5435ce38794d80dcefa2e40dd85be377299
Mithun Dhanasuthan <85911312+mithustar39@users.noreply.github.com>
2024-08-14 17:27:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import { AuthProvider } from './contexts/AuthContext';
+import App from '../App';
+import { AuthProvider } from '../contexts/AuthContext';
 
 ReactDOM.render(
   <React.StrictMode>
